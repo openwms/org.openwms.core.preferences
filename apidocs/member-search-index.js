a007cd7ef32a0725a634ed049fd29dc44370256d
CI Server <ci@example.com>
2023-09-29 14:15:15 +0000

    Site checkin for project OpenWMS.org CORE: Preferences

---
+++ apidocs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"org.openwms.core.preferences","c":"PreferencesStarter","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"org.openwms.core.preferences","c":"PreferencesStarter","l":"PreferencesStarter()","u":"%3Cinit%3E()"}];updateSearchResults();
---
+++ apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.openwms.core.preferences","l":"PreferencesStarter"}];updateSearchResults();
