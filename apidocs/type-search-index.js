d6f5a74df969d80a454c1da58c976be058268f99
CI Server <ci@example.com>
2026-02-12 13:54:27 +0000

    Site checkin for project OpenWMS.org CORE: Preferences

--- apidocs/type-search-index.js
+++ apidocs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.openwms.core.preferences","l":"PreferencesStarter"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.openwms.core.preferences","l":"PreferencesJpaConfiguration"},{"p":"org.openwms.core.preferences","l":"PreferencesStarter"}];updateSearchResults();
